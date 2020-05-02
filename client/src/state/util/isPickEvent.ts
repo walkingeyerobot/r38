7296fdd7f8bb6320946647ca96efc709267aa7be
Ned Burns <net7runner@gmail.com>
2020-05-02 18:14:51 -0400

    [replay] Add clickable timeline
    Allows for quick access to specific packs and picks.
    
    Also moves the synchronized timeline option into the timeline view.

--- client/src/state/util/isPickEvent.ts
+++ client/src/state/util/isPickEvent.ts
@@ -1,6 +1,15 @@
-import { TimelineEvent } from '../../draft/TimelineEvent';
+import { TimelineEvent, ActionMoveCard } from '../../draft/TimelineEvent';
 import { find } from '../../util/collection';
 
 export function isPickEvent(event: TimelineEvent): boolean {
   return find(event.actions, { type: 'move-card' }) != -1;
+}
+
+export function getPickAction(event: TimelineEvent): ActionMoveCard | null {
+  const index = find(event.actions, { type: 'move-card' });
+  if (index != -1) {
+    return event.actions[index] as ActionMoveCard;
+  } else {
+    return null;
+  }
 }
