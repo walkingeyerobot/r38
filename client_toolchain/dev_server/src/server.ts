ad0ddd14860eba0e85224e298a4125b0ec2c1118
Ned Burns <net7runner@gmail.com>
2020-07-10 01:07:20 -0400

    Change the dev server to proxy almost all traffic to the go server
    The dev server now proxies all traffic that isn't part of the compiled
    app bundle straight to the go server. We don't bother with trying to
    stub out replies now -- that's what the dev DB is for.

--- client_toolchain/dev_server/src/server.ts
+++ client_toolchain/dev_server/src/server.ts
@@ -2,18 +2,15 @@ import express from 'express';
 import webpack from 'webpack';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
-import bodyParser from 'body-parser';
-import { serveIndex } from './route/serveIndex';
-import { configureApiRoutes } from './route/configureApiRoutes';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 const webpackConfig = require('../../config/webpack.dev');
 
 function main() {
   let app = express();
 
-  configureExpress(app);
-  configureRoutes(app);
-  configureApiRoutes(app, true);
+  configureDevServing(app);
+  configureProxy(app);
 
   app.listen(8080, 'localhost', () => {
     console.log('Listening on http://localhost:8080');
@@ -21,11 +18,7 @@ function main() {
   });
 }
 
-function configureExpress(app: express.Express) {
-  // TODO: Re-enable this if we need it and if we can figure out how to let the
-  // proxy have higher priority
-  // app.use(bodyParser.json());
-
+function configureDevServing(app: express.Express) {
   const compiler = webpack(webpackConfig);
   app.use(webpackDevMiddleware(compiler, {
     publicPath: webpackConfig.output.publicPath,
@@ -34,21 +27,11 @@ function configureExpress(app: express.Express) {
   app.use(webpackHotMiddleware(compiler));
 }
 
-function configureRoutes(app: express.Express) {
-  for (let htmlPath of HTML_PATHS) {
-    app.get(htmlPath, serveIndex);
-  }
+function configureProxy(app: express.Express) {
+  app.use('/', createProxyMiddleware({
+    target: 'http://localhost:12270',
+    changeOrigin: true,
+  }));
 }
-
-const HTML_PATHS = [
-  '/',
-  '/login',
-
-  '/draft/:id',
-  '/draft/:id/*',
-
-  '/replay/*',
-  '/deckbuilder/*',
-]
 
 main();
