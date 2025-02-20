c0d8192fbf38cbf8513a23418827699421c2645a
Gus Prevas <kprevas@gmail.com>
2025-02-19 21:28:42 -0500

    adds undo pick endpoint

--- main_test.go
+++ main_test.go
@@ -25,17 +25,15 @@ func ptr[T any](v T) *T {
 
 var SEED = 677483
 
-func TestInPersonDraft(t *testing.T) {
+func doSetup(t *testing.T) (net.Listener, *sql.DB, error) {
 	xsrfKey = "test"
 	sock = "test.sock"
 
 	unix.Unlink(sock)
 	filterListener, err := net.Listen("unix", sock)
-	defer func() { unix.Unlink(sock) }()
 	if err != nil {
 		t.Error(err)
 	}
-	defer filterListener.Close()
 	go func() {
 		for {
 			conn, filterErr := filterListener.Accept()
@@ -65,9 +63,12 @@ func TestInPersonDraft(t *testing.T) {
 		t.Error(err)
 	}
 
-	var tx *sql.Tx
-
 	rand.Seed(int64(SEED))
+	return filterListener, db, err
+}
+
+func makeDraft(t *testing.T, err error, db *sql.DB) {
+	var tx *sql.Tx
 
 	tx, err = db.Begin()
 	if err != nil {
@@ -103,11 +104,11 @@ func TestInPersonDraft(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
+}
 
-	handlers := NewHandler(db, false)
-
-	players := rand.Perm(12)
-	seats := rand.Perm(8)
+func populateDraft(t *testing.T, handlers http.Handler) (players []int, seats []int) {
+	players = rand.Perm(12)
+	seats = rand.Perm(8)
 	for seat := range 8 {
 		t.Logf("joining seat %d as player %d", seats[seat]+1, players[seat]+1)
 		w := httptest.NewRecorder()
@@ -120,47 +121,67 @@ func TestInPersonDraft(t *testing.T) {
 			t.Errorf("player joining draft failed: %s", body)
 		}
 	}
+	return
+}
+
+func findCardToPick(t *testing.T, db *sql.DB, player int, round int, card int) string {
+	var rows *sql.Rows
+	var err error
+	if card == 0 {
+		query := `select cards.cardId from cards 
+								join packs on packs.id = cards.pack
+								join seats on seats.id = packs.seat
+								where seats.position = 8`
+		rows, err = db.Query(query)
+	} else {
+		query := `select cards.cardId from cards
+								join v_packs on v_packs.id = cards.pack
+								join seats on seats.id = v_packs.seat
+								where seats.user = ?
+								and v_packs.round = ?
+								and v_packs.count = ?`
+		rows, err = db.Query(query, player, round+1, 15-card)
+	}
+	if err != nil {
+		t.Errorf("error finding card to pick for player %d, pack %d, pick %d: %s",
+			player, round+1, card+1, err.Error())
+	}
+
+	var cardId string
+	var cards []string
+	for rows.Next() {
+		err = rows.Scan(&cardId)
+		if err != nil {
+			t.Errorf("error finding card to pick for player %d, pack %d, pick %d: %s",
+				player, round+1, card+1, err.Error())
+		}
+		cards = append(cards, cardId)
+	}
+	if len(cards) == 0 {
+		t.Errorf("no cards in pack for player %d, pack %d, pick %d", player, round+1, card+1)
+	}
+	cardId = cards[rand.Intn(len(cards))]
+	return cardId
+}
+
+func TestInPersonDraft(t *testing.T) {
+	filterListener, db, err := doSetup(t)
+	defer func() { unix.Unlink(sock) }()
+	defer filterListener.Close()
+	defer db.Close()
+
+	makeDraft(t, err, db)
+
+	handlers := NewHandler(db, false)
+
+	players, seats := populateDraft(t, handlers)
 
 	for round := range 3 {
 		for card := range 15 {
 			for _, seat := range rand.Perm(8) {
 				player := players[seat] + 1
 
-				var rows *sql.Rows
-				if card == 0 {
-					query := `select cards.cardId from cards 
-								join packs on packs.id = cards.pack
-								join seats on seats.id = packs.seat
-								where seats.position = 8`
-					rows, err = db.Query(query)
-				} else {
-					query := `select cards.cardId from cards
-								join v_packs on v_packs.id = cards.pack
-								join seats on seats.id = v_packs.seat
-								where seats.user = ?
-								and v_packs.round = ?
-								and v_packs.count = ?`
-					rows, err = db.Query(query, player, round+1, 15-card)
-				}
-				if err != nil {
-					t.Errorf("error finding card to pick for player %d, pack %d, pick %d: %s",
-						player, round+1, card+1, err.Error())
-				}
-
-				var cardId string
-				var cards []string
-				for rows.Next() {
-					err = rows.Scan(&cardId)
-					if err != nil {
-						t.Errorf("error finding card to pick for player %d, pack %d, pick %d: %s",
-							player, round+1, card+1, err.Error())
-					}
-					cards = append(cards, cardId)
-				}
-				if len(cards) == 0 {
-					t.Errorf("no cards in pack for player %d, pack %d, pick %d", player, round+1, card+1)
-				}
-				cardId = cards[rand.Intn(len(cards))]
+				cardId := findCardToPick(t, db, player, round, card)
 
 				token := xsrftoken.Generate(xsrfKey, strconv.FormatInt(int64(player), 16), "pick1")
 
@@ -177,5 +198,172 @@ func TestInPersonDraft(t *testing.T) {
 			}
 
 		}
+	}
+}
+
+func TestInPersonDraftUndoFirstPick(t *testing.T) {
+	filterListener, db, err := doSetup(t)
+	defer func() { unix.Unlink(sock) }()
+	defer filterListener.Close()
+	defer db.Close()
+
+	makeDraft(t, err, db)
+
+	handlers := NewHandler(db, false)
+
+	players, seats := populateDraft(t, handlers)
+
+	player := players[0] + 1
+	cardRfid := findCardToPick(t, db, player, 0, 0)
+	row := db.QueryRow(`select id, pack from cards where cardid = ?`, cardRfid)
+	var cardId int64
+	var origPackID int64
+	err = row.Scan(&cardId, &origPackID)
+	if err != nil {
+		t.Errorf("couldn't find picked card ID: %s", err.Error())
+	}
+	row = db.QueryRow(`select id from seats where position = ?`, seats[0])
+	var origSeatID int64
+	err = row.Scan(&origSeatID)
+	if err != nil {
+		t.Errorf("couldn't find pack seat ID: %s", err.Error())
+	}
+
+	token := xsrftoken.Generate(xsrfKey, strconv.FormatInt(int64(player), 16), "pick1")
+
+	handlers.ServeHTTP(httptest.NewRecorder(),
+		httptest.NewRequest("POST", fmt.Sprintf("/api/pickrfid/?as=%d", player),
+			strings.NewReader(fmt.Sprintf(`{"draftId": 1, "cardRfids": ["%s"], "xsrfToken": "%s"}`, cardRfid, token))))
+
+	w := httptest.NewRecorder()
+	handlers.ServeHTTP(w,
+		httptest.NewRequest("POST", fmt.Sprintf("/api/undopick/?as=%d", player),
+			strings.NewReader(fmt.Sprintf(`{"draftId": 1, "xsrfToken": "%s"}`, token))))
+	res := w.Result()
+	if res.StatusCode != http.StatusOK {
+		body, _ := io.ReadAll(res.Body)
+		t.Errorf("undo failed: %s", body)
+	}
+
+	row = db.QueryRow(`select count(*) from events where draft = 1 and position = ? and card1 = ?`,
+		seats[0], cardId)
+	var eventsCount int64
+	err = row.Scan(&eventsCount)
+	if err != nil {
+		t.Errorf("couldn't execute events count query: %s", err.Error())
+	}
+	if eventsCount != 0 {
+		t.Error("didn't delete event")
+	}
+
+	row = db.QueryRow(`select pack from cards where id = ?`, cardId)
+	var packID int64
+	err = row.Scan(&packID)
+	if err != nil {
+		t.Errorf("couldn't execute pack query: %s", err.Error())
+	}
+	if packID != origPackID {
+		t.Errorf("didn't reset card's pack (was %d, expected %d)", packID, origPackID)
+	}
+
+	row = db.QueryRow(`select seat from packs where id = ?`, origPackID)
+	var seatID int64
+	err = row.Scan(&seatID)
+	if err != nil {
+		t.Errorf("couldn't execute seat query: %s", err.Error())
+	}
+	if seatID != origSeatID {
+		t.Errorf("didn't reset pack's seat (was %d, expected %d)", seatID, origSeatID)
+	}
+}
+
+func TestInPersonDraftUndoSubsequentPick(t *testing.T) {
+	filterListener, db, err := doSetup(t)
+	defer func() { unix.Unlink(sock) }()
+	defer filterListener.Close()
+	defer db.Close()
+
+	makeDraft(t, err, db)
+
+	handlers := NewHandler(db, false)
+
+	players, seats := populateDraft(t, handlers)
+
+	player := players[0] + 1
+
+	for card := range 5 {
+		for _, seat := range rand.Perm(8) {
+			player := players[seat] + 1
+
+			cardId := findCardToPick(t, db, player, 0, card)
+
+			token := xsrftoken.Generate(xsrfKey, strconv.FormatInt(int64(player), 16), "pick1")
+
+			handlers.ServeHTTP(httptest.NewRecorder(),
+				httptest.NewRequest("POST", fmt.Sprintf("/api/pickrfid/?as=%d", player),
+					strings.NewReader(fmt.Sprintf(`{"draftId": 1, "cardRfids": ["%s"], "xsrfToken": "%s"}`, cardId, token))))
+		}
+	}
+
+	cardRfid := findCardToPick(t, db, player, 0, 5)
+	row := db.QueryRow(`select id, pack from cards where cardid = ?`, cardRfid)
+	var cardId int64
+	var origPackID int64
+	err = row.Scan(&cardId, &origPackID)
+	if err != nil {
+		t.Errorf("couldn't find picked card ID: %s", err.Error())
+	}
+	row = db.QueryRow(`select id from seats where position = ?`, seats[0])
+	var origSeatID int64
+	err = row.Scan(&origSeatID)
+	if err != nil {
+		t.Errorf("couldn't find pack seat ID: %s", err.Error())
+	}
+
+	token := xsrftoken.Generate(xsrfKey, strconv.FormatInt(int64(player), 16), "pick1")
+
+	handlers.ServeHTTP(httptest.NewRecorder(),
+		httptest.NewRequest("POST", fmt.Sprintf("/api/pickrfid/?as=%d", player),
+			strings.NewReader(fmt.Sprintf(`{"draftId": 1, "cardRfids": ["%s"], "xsrfToken": "%s"}`, cardRfid, token))))
+
+	w := httptest.NewRecorder()
+	handlers.ServeHTTP(w,
+		httptest.NewRequest("POST", fmt.Sprintf("/api/undopick/?as=%d", player),
+			strings.NewReader(fmt.Sprintf(`{"draftId": 1, "xsrfToken": "%s"}`, token))))
+	res := w.Result()
+	if res.StatusCode != http.StatusOK {
+		body, _ := io.ReadAll(res.Body)
+		t.Errorf("undo failed: %s", body)
+	}
+
+	row = db.QueryRow(`select count(*) from events where draft = 1 and position = ? and card1 = ?`,
+		seats[0], cardId)
+	var eventsCount int64
+	err = row.Scan(&eventsCount)
+	if err != nil {
+		t.Errorf("couldn't execute events count query: %s", err.Error())
+	}
+	if eventsCount != 0 {
+		t.Error("didn't delete event")
+	}
+
+	row = db.QueryRow(`select pack from cards where id = ?`, cardId)
+	var packID int64
+	err = row.Scan(&packID)
+	if err != nil {
+		t.Errorf("couldn't execute pack query: %s", err.Error())
+	}
+	if packID != origPackID {
+		t.Errorf("didn't reset card's pack (was %d, expected %d)", packID, origPackID)
+	}
+
+	row = db.QueryRow(`select seat from packs where id = ?`, origPackID)
+	var seatID int64
+	err = row.Scan(&seatID)
+	if err != nil {
+		t.Errorf("couldn't execute seat query: %s", err.Error())
+	}
+	if seatID != origSeatID {
+		t.Errorf("didn't reset pack's seat (was %d, expected %d)", seatID, origSeatID)
 	}
 }
