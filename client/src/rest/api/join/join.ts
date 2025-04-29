a17843ac85c752976b99cc3fa43ac2e0e58bfb85
Ned Burns <net7runner@gmail.com>
2025-04-28 23:20:33 -0400

    Update table seating view to support joining drafts

--- client/src/rest/api/join/join.ts
+++ client/src/rest/api/join/join.ts
@@ -3,9 +3,12 @@ import { endpoint } from "@/rest/endpoint";
 export const ROUTE_JOIN_DRAFT = endpoint({
   method: "post",
   route: "/api/join/",
-  response: {},
+  queryVars: {
+    as: 0,
+  } as { as?: number },
   bodyVars: {
     id: 0 as number,
     position: undefined as number | undefined,
   },
+  response: {},
 });
