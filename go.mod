4e4840eca7eb4706510d4f843840945850a0b2e8
Gus Prevas <kprevas@gmail.com>
2025-05-17 11:17:44 -0400

    objectbox: schema

--- go.mod
+++ go.mod
@@ -5,10 +5,13 @@ go 1.23.6
 require (
 	github.com/BurntSushi/migration v0.0.0-20140125045755-c45b897f1335
 	github.com/bwmarrin/discordgo v0.28.1
+	github.com/davecgh/go-spew v1.1.1
 	github.com/go-co-op/gocron v1.37.0
+	github.com/google/flatbuffers v23.5.26+incompatible
 	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510
 	github.com/gorilla/sessions v1.4.0
 	github.com/mattn/go-sqlite3 v1.14.24
+	github.com/objectbox/objectbox-go v1.9.0
 	golang.org/x/net v0.35.0
 	golang.org/x/oauth2 v0.26.0
 )
@@ -17,6 +20,7 @@ require (
 	github.com/google/uuid v1.4.0 // indirect
 	github.com/gorilla/securecookie v1.1.2 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/objectbox/objectbox-generator/v4 v4.0.0 // indirect
 	github.com/robfig/cron/v3 v3.0.1 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	golang.org/x/crypto v0.33.0 // indirect
---
+++ schema/schema.go
@@ -0,0 +1,57 @@
+package schema
+
+//go:generate go run github.com/objectbox/objectbox-go/cmd/objectbox-gogen
+
+type Card struct {
+	Id     uint64
+	Data   string
+	CardId string
+}
+
+type Draft struct {
+	Id              uint64
+	Name            string
+	Format          string
+	InPerson        bool
+	Seats           []*Seat
+	UnassignedPacks []*Pack
+	Events          []*Event
+}
+
+type Pack struct {
+	Id            uint64
+	Round         int
+	OriginalCards []*Card
+	Cards         []*Card
+}
+
+type Seat struct {
+	Id            uint64
+	Position      int
+	User          *User `objectbox:"link"`
+	ReservedUser  *User `objectbox:"link"`
+	ScanSound     int
+	ErrorSound    int
+	Round         int
+	Packs         []*Pack
+	OriginalPacks []*Pack
+	PickedCards   []*Card
+}
+
+type User struct {
+	Id          uint64
+	DiscordId   string `objectbox:"unique"`
+	DiscordName string
+	MtgoName    string
+	Picture     string
+}
+
+type Event struct {
+	Id           uint64
+	Position     int
+	Announcement string
+	Card1        *Card `objectBox:"link"`
+	Card2        *Card `objectBox:"link"`
+	Modified     int
+	Round        int
+}
